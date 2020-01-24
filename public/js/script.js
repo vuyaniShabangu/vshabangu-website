16a22bcb9a4364f256121dce89884bbd3207af4f
Vuyani Shabangu <vuyani@jemstep.com>
2020-01-24 09:31:36 +0200

    adding resume info

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+    const openNav = () => {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+    
+    const closeNav = () => {
+      document.getElementById("mySidenav").style.width = "0";
+    }
